8f736c64475a5d8302fdb80c9ad968ce7b0814d6
Benjamin-25 <pabagonzalezbenjamin@gmail.com>
2025-07-07 21:24:28 -0500

    my fist commit and java project implementation

---
+++ src/main/java/com/servicioWeb/servicioWeb/controllers/usuarioController.java
@@ -0,0 +1,16 @@
+package com.servicioWeb.servicioWeb.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//con esto definimos que nuestra clase es un controlador, para poder manejar Url en nuestro servicio o app web
+@RestController
+public class usuarioController {
+
+    @RequestMapping(value = "prueba")
+    public List<String> saludo(){
+        return List.of("Benjamin", " Ana", "Angie", "Tatiana");
+    }
+}
