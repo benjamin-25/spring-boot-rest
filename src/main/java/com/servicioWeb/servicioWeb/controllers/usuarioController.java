ba6d6bfe786e51faffe47da739744e6fe41b7639
Benjamin-25 <pabagonzalezbenjamin@gmail.com>
2025-08-19 22:20:55 -0500

    add test to create a new user with the user model and consume the test endpoint

--- src/main/java/com/servicioWeb/servicioWeb/controllers/usuarioController.java
+++ src/main/java/com/servicioWeb/servicioWeb/controllers/usuarioController.java
@@ -1,5 +1,6 @@
 package com.servicioWeb.servicioWeb.controllers;
 
+import com.servicioWeb.servicioWeb.models.UsuarioModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,16 @@ import java.util.List;
 //con esto definimos que nuestra clase es un controlador, para poder manejar Url en nuestro servicio o app web
 @RestController
 public class usuarioController {
+
+    @RequestMapping(value = "getUser")
+    public UsuarioModel getUsuario(){
+        UsuarioModel usuario = new UsuarioModel();
+        usuario.setNombre("Benjamin");
+        usuario.setApellido("Prueba");
+        usuario.setCorreo("benja@gmail.com");
+        usuario.setPassword("Bapg123456789*");
+        return usuario;
+    }
 
     @RequestMapping(value = "prueba")
     public List<String> saludo(){
